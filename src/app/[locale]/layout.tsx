c66fe7142aa4ed3b0ee396e64b77d07098ff3e87
xiaowei <fanhua@fandeMacBook-Pro.local>
2025-11-13 11:21:16 +0800

    feat: enable analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import {NextIntlClientProvider} from 'next-intl';
 import {getMessages} from 'next-intl/server';
 import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
+import {Analytics} from '@vercel/analytics/react';
 import '../globals.css';
 
 export function generateStaticParams() {
@@ -42,6 +43,7 @@ export default async function LocaleLayout({
         <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
+        <Analytics />
       </body>
     </html>
   );
